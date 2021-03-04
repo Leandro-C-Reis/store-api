6ca810a2f96e3efd3b53f447d6d6a488b1ac620d
Leandro Christian dos Reis <leandroreis02@gmail.com>
2021-03-04 15:40:25 -0300

    fix(orm): ORM connection changed

--- src/loaders/connection.ts
+++ src/loaders/connection.ts
@@ -1,5 +1,28 @@
 import { createConnection } from 'typeorm';
+import * as variables from '../config/variables';
 
-console.log('ORM connection created');
+const connection = async () => {
+    const connect = await createConnection({
+        type: 'mysql',
+        host: variables.db_host,
+        port: variables.db_port,
+        database: variables.db_name,
+        username: variables.db_user,
+        password: variables.db_password,
+        entities: ["./src/database/models/*.{ts,js}"],
+        migrations: ["./src/database/migrations/*.{ts,js}"],
+        cli: {
+            entitiesDir: "./src/database/models",
+            migrationsDir: "./src/database/migrations"
+        }
+    });
 
-export default createConnection();
+    if (!connect.isConnected)
+    {
+        process.exit(1);
+    }
+    
+    console.log('ORM connection created');
+}   
+
+export default connection;
--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,3 +1,3 @@
 import connection from './connection';
 
-connection;
+connection();
