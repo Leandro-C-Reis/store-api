c6f9492abcb650d8b5fc655993dc5d78b2f5877e
Leandro Christian dos Reis <leandroreis02@gmail.com>
2021-03-10 12:43:32 -0300

    build(products): products controller finished

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -1,30 +1,92 @@
 import { Request, Response } from 'express';
-
+import Products from '../services/Products';
+import Joi from 'joi';
 
 export default class ProductsController {
 
     public static async index(request: Request, response: Response)
     {
+        const products = await Products.getAll();
 
+        return response.json(products);
     }
 
     public static async show(request: Request, response: Response)
     {
-        
+        const id = parseInt(request.params.id);
+
+        if (!id)
+        {
+            return response.json({ error: "ID inválido!" });
+        }
+
+        const product = await Products.getOne(id);
+
+        return response.json(product);
     }
 
     public static async create(request: Request, response: Response)
     {
+        delete request.body.user;
+
+        const schema = Joi.object({
+            name: Joi.string().required(),
+            description: Joi.string().required().max(255),
+            value: Joi.number().required()
+        });
+
+        const validate = schema.validate(request.body)
         
+        if (validate.error)
+        {
+            return response.json(validate.error);
+        }
+
+        const product = await Products.create(request.body);
+
+        return response.json(product);
     }
 
     public static async update(request: Request, response: Response)
     {
+        delete request.body.user;
+
+        const id = parseInt(request.params.id);
+
+        if (!id)
+        {
+            return response.json({ error: "ID inválido!" });
+        }
+
+        const schema = Joi.object({
+            name: Joi.string(),
+            description: Joi.string().max(255),
+            value: Joi.number()
+        });
+
+        const validate = schema.validate(request.body)
         
+        if (validate.error)
+        {
+            return response.json(validate.error);
+        }
+
+        const updated = await Products.update(request.body, id);
+
+        return response.json(updated);
     }
 
     public static async delete(request: Request, response: Response)
     {
-        
+        const id = parseInt(request.params.id);
+
+        if (!id)
+        {
+            return response.json({ error: "ID inválido!" });
+        }
+
+        const deleted = await Products.delete(id);
+
+        return response.json(deleted);
     }
 }
